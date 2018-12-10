854ee20e9f55c80c3af2c5620d20dc89519a2435
arifprasetiyo <45700541+arifprasetiyo@users.noreply.github.com>
2018-12-11 00:02:42 +0700

    Update MLapangan.vb

--- MLapangan.vb
+++ MLapangan.vb
@@ -2,11 +2,11 @@
 
 Public Class MLapangan
     Private Sub bTambah_Click(sender As Object, e As EventArgs) Handles bTambah.Click
-        TD_Lapangan.Show()
-    End Sub
+        TD_Lapangan.Show()                                                        
+    End Sub                                                                       
 
     Private Sub MLapangan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        koneksi()
+        koneksi()                                                               
         sql = "select * from lapangan order by kd_lapangan"
         adaptor = New SqlDataAdapter(sql, conn)
         data = New DataSet
