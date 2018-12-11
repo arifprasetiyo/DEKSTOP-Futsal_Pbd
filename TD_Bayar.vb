8d8867c64e70b862e5f0ba323b2f4646f14349e2
arifprasetiyo <45700541+arifprasetiyo@users.noreply.github.com>
2018-12-11 10:36:23 +0700

    Update TD_Bayar.vb

--- TD_Bayar.vb
+++ TD_Bayar.vb
@@ -1,9 +1,9 @@
 ﻿Public Class TD_Bayar
     Private Sub BunifuFlatButton1_Click(sender As Object, e As EventArgs) Handles BunifuFlatButton1.Click
         Me.Hide()
-        BunifuMaterialTextbox1.Text = ""
-        BunifuMaterialTextbox2.Text = ""
-        BunifuMaterialTextbox3.Text = ""
+        BunifuMaterialTextbox1.Text = ""                                                                                
+        BunifuMaterialTextbox2.Text = ""                                                                            
+        BunifuMaterialTextbox3.Text = ""                                                                                         
    
     End Sub
 End Class
